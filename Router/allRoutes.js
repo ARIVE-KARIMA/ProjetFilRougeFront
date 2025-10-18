fdd0c3cc27adaf10879f51a7aed55cb33918e8fd
karima Arive <karima.arive@gmail.com>
2025-10-18 15:17:51 +0200

    Ajout pages connexion + inscription

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,7 +4,10 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/galerie", "La galerie", "/pages/galerie.html"),
+    new Route("/signin", "connexion", "/pages/signin.html"),
+    new Route("/signup", "inscription", "/pages/signup.html"),
 ];
+
 
 // Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
